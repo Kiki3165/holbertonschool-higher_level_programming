b844b05b2b8a5ad277e8469a3776e87fae69fa01
kiki3165 <kikikou82@gmail.com>
2023-01-19 17:28:32 +0000

    fixpermi

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,9 +5,9 @@ if __name__ == "__main__":
     argv = sys.argv[1:]
 
 if len(argv) == 0:
-    print("1 argument: 0.")
+    print("1 argument.")
 else:
-    print(f"Number of argument(s): {len(argv)}", end=':' if len(argv) != 1 else '.')
+    print(f"{len(argv)} arguments {len(argv)}", end=':' if len(argv) != 1 else '.')
     print()
     for i, arg in enumerate(argv, start=1):
         print(f"{i}: {arg}")
