00ecf3b0177dc0894042ff41ba8b3a37d9b9e4d4
kiki3165 <kikikou82@gmail.com>
2023-01-19 17:14:55 +0000

    task2

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+import sys
+
+argv = sys.argv[1:]
+
+if len(argv) == 0:
+    print("Number of argument(s): 0.")
+else:
+    print(f"Number of argument(s): {len(argv)}", end=':' if len(argv) != 1 else '.')
+    print()
+    for i, arg in enumerate(argv, start=1):
+        print(f"{i}: {arg}")
