cba5bc6d4d611479aaaa5aebdb612c36816cfb19
kiki3165 <kikikou82@gmail.com>
2023-01-19 17:11:56 +0000

    task1

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -3,7 +3,7 @@ from calculator_1 import add, sub, mul, div
 if __name__ == "__main__":
     a = 10
     b = 5
-    print("{10} + {5} = {15}".format(a, b, add(a, b)))
-    print("{10} - {5} = {5}".format(a, b, sub(a, b)))
-    print("{10} * {5} = {50}".format(a, b, mul(a, b)))
-    print("{10} / {5} = {2}".format(a, b, div(a, b)))
+    print("{0} + {1} = {2}".format(a, b, add(a, b)))
+    print("{0} - {1} = {2}".format(a, b, sub(a, b)))
+    print("{0} * {1} = {2}".format(a, b, mul(a, b)))
+    print("{0} / {1} = {2}".format(a, b, div(a, b)))
