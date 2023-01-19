4f43a876063390685141616bb55a77b105c6a3a3
kiki3165 <kikikou82@gmail.com>
2023-01-19 17:06:21 +0000

    task1

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -7,8 +7,8 @@ if __name__ == "__main__":
     result2 = sub(a, b)
     result3 = mul(a, b)
     result4 = div(a, b)
-    print("{a} + {b} = {result1}".format(a, b, add(a, b)))
-    print("{a} - {b} = {result2}".format(a, b, sub(a, b)))
-    print("{a} * {b} = {result3}".format(a, b, mul(a, b)))
-    print("{a} / {b} = {result4}".format(a, b, div(a, b)))
+    print("{10} + {5} = {result1}".format(a, b, add(a, b)))
+    print("{10} - {5} = {result2}".format(a, b, sub(a, b)))
+    print("{10} * {5} = {result3}".format(a, b, mul(a, b)))
+    print("{10} / {5} = {result4}".format(a, b, div(a, b)))
     
