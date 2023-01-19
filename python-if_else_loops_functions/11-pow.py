66765e43b649688f31c9f58b3a6af15c6d3a9d46
kiki3165 <kikikou82@gmail.com>
2023-01-19 16:20:07 +0000

    task11

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
 def pow(a, b):
-    if b == 0:
-        return 1
-    elif a == 0:
-        return 0
-    elif b == 1:
-        return a
-    else:
-        return a*pow(a, b-1)
-    print(pow(a, b))
+    return a ** b
