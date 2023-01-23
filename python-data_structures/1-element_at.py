607c3d0801268e615b6ce90e42772661e35ed0af
kiki3165 <kikikou82@gmail.com>
2023-01-23 14:24:13 +0000

    task1

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -6,4 +6,4 @@ def element_at(my_list, idx):
     if idx > my_list:
         return None
     for i in my_list:
-        print(f"Element at index {idx} is {my_list}")
+        print("Element at index {:d} is {}".format(idx, element_at(my_list, idx)))
