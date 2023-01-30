dfc585b874e6c026f692e7e7dd4a2797428e66db
kiki3165 <kikikou82@gmail.com>
2023-01-30 16:18:32 +0000

    adddocu

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-
+'''def class'''
 class Square:
+    '''function'''
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
