1ee33b4d94e7043026828f646ed5492a8b6187fd
kiki3165 <kikikou82@gmail.com>
2023-01-30 17:11:41 +0000

    fixpep8

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 '''def class'''
+
+
 class Square:
     '''function'''
     def __init__(self, size=0):
--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 '''def class'''
+
+
 class Square:
     '''simple def'''
     def __init__(self, size=0):
