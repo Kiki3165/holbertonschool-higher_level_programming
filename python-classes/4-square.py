c8e6373ffd2d959c4ac4f48881a7c5819395912e
kiki3165 <kikikou82@gmail.com>
2023-01-30 16:51:53 +0000

    task4

---
+++ python-classes/4-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''def class'''
+class Square:
+    '''simple def'''
+    def __init__(self, size=0):
+        self.size = size
+    @property
+    def size(self):
+        return self.__size
+    
+    '''simple def'''
+    def area(self):
+        return self.__size ** 2
+
+    '''simple def'''
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
