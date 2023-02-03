d2cc5a969207bf325128f40ce5d1339e947d44cd
kiki3165 <kikikou82@gmail.com>
2023-02-03 14:00:42 +0000

    blank line

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -7,7 +7,7 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
-        
+
     @property
     def width(self):
         return self.__width
@@ -19,7 +19,7 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
-        
+
     @property
     def height(self):
         return self.__height
@@ -34,7 +34,7 @@ class Rectangle:
 
     def area(self):
         return self.width * self.height
-    
+
     def perimeter(self):
         if self.width == 0 or self.height == 0:
             return 0
