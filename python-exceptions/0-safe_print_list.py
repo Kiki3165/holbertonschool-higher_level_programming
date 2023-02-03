defb748cff01d40890cb6ea618aa7fa904bc0221
kiki3165 <kikikou82@gmail.com>
2023-02-03 22:39:57 +0000

    end ""

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
     elements_printed = 0
     try:
         for i in range(x):
-            print(my_list[i])
+            print(my_list[i], end="")
             elements_printed += 1
         print()
     except:
