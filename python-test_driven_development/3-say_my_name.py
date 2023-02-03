d95c09d24f2219257b757748db114491f2172469
kiki3165 <kikikou82@gmail.com>
2023-02-03 19:14:25 +0000

    task3

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''simple def'''
+
+
+def say_my_name(first_name, last_name=""):
+    if not isinstance(first_name, str) or not isinstance(last_name, str):
+        raise TypeError("first_name must be a string or last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
