bae017ab8e7726018b7687a69f70460c04b5cafa
kiki3165 <kikikou82@gmail.com>
2023-02-08 15:42:20 +0000

    documentation

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -8,12 +8,15 @@ Rectangle = __import__("8-rectangle.py").Rectangle
 class Square(Rectangle):
     '''def'''
     def __init__(self, size):
+        '''def'''
         self.integer_validator("size", size)
         self.__size = size
         super().__init__(size, size)
 
     def area(self):
+        '''return'''
         return self.__size ** 2
 
     def __str__(self):
+        '''return'''
         return "[Square] {}/{}".format(self.__size, self.__size)
