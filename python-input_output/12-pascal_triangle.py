095d88bff17f7641f2f0ee94c8d88d1b10b8efe6
kiki3165 <kikikou82@gmail.com>
2023-02-10 21:42:01 +0000

    task12

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -11,7 +11,7 @@ def pascal_triangle(n):
     current_row = [1]
     for i in range(n):
         triangle.append(current_row)
-        current_row = [1] + [current_row[j] + current_row[j - 1] 
+        current_row = [1] + [current_row[j] + current_row[j - 1]
                              for j in range(1, len(current_row))] + [1]
 
     return triangle
