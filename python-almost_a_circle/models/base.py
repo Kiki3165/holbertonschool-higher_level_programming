58c3cd0a130570dc3d1af7f7adff0d2828b0bbc4
kiki3165 <kikikou82@gmail.com>
2023-02-14 11:35:13 +0000

    task1

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''def class'''
+
+
+class base:
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        '''constructor'''
+        if id is not None:
+            self.id = id
+        else:
+            self.__class__.__nb_objects += 1
+            self.id = self.__class__.__nb_objects
