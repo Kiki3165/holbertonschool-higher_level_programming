2e68dbaee3cb575778f263caa219e8386d36b4a8
kiki3165 <kikikou82@gmail.com>
2023-02-15 09:09:18 +0000

    add display and area

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -18,3 +18,14 @@ class Square(Rectangle):
     def __str__(self):
         '''def str'''
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}"
+
+    def display(self):
+        '''def display'''
+        for i in range(self.y):
+            print()
+        for i in range(self.height):
+            print(" " * self.x + "#" * self.width)
+
+    def area(self):
+        '''def area'''
+        return self.__width * self.__height
