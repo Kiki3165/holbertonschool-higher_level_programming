09b0229b752942286ae9b52b01ffe98d2e554ff5
kiki3165 <kikikou82@gmail.com>
2023-02-15 16:52:38 +0000

    change code

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -7,24 +7,18 @@ from models.rectangle import Rectangle
 class Square(Rectangle):
     '''class square'''
     def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
         '''def'''
-        super().__init__(id)
-        self.width = size
-        self.height = size
-        self.x = x
-        self.y = y
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        '''def'''
+        self.width = value
+        self.height = value
 
     def __str__(self):
-        '''def str'''
-        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.__height} - {self.__width}"
-
-    def display(self):
-        '''def display'''
-        for i in range(self.y):
-            print()
-        for i in range(self.height):
-            print(" " * self.x + "#" * self.width)
-
-    def area(self):
-        '''def area'''
-        return self.__width * self.__height
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
