fee910ef4ec59867cf6521749c8eb28641bb45cc
kiki3165 <kikikou82@gmail.com>
2023-02-15 18:23:30 +0000

    add func save_to_file

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -24,3 +24,13 @@ class Base:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        '''def'''
+        filename = cls.__name__ + ".json"
+        if list_objs is None:
+            list_objs = []
+        with open(filename, mode='w', encoding='utf-8') as file:
+            list_dict = [obj.to_dictionary() for obj in list_objs]
+            file.write(cls.to_json_string(list_dict))
