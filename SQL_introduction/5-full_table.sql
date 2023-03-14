0db186d5e771fa523f1becb2735baee7eb9dae8b
kiki3165 <kikikou82@gmail.com>
2023-03-14 14:31:42 +0000

    test

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,6 +1,5 @@
 -- 3 first students IN the Batch ID=3 
 -- because Batch 3 is the best!
-SELECT column_name, data_type, character_maximum_length, is_nullable 
-FROM information_schema.columns 
-WHERE table_name = 'first_table' 
-  AND table_schema = 'hbtn_0c_0';
+SELECT COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH, IS_NULLABLE, COLUMN_DEFAULT, COLUMN_KEY, EXTRA 
+FROM INFORMATION_SCHEMA.COLUMNS 
+WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
