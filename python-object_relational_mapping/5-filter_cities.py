f32b0e169c184313451903e508919338a6e03494
kiki3165 <kikikou82@gmail.com>
2023-03-17 20:44:05 +0000

    task5-6

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,30 +1,32 @@
 #!/usr/bin/python3
 '''module'''
-import MySQLdb
-import sys
-
 if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        print("Usage: {} <MySQL username> <MySQL password> <database name> <state name>".format(sys.argv[0]))
-        sys.exit(1)
+    from sys import argv
+    import MySQLdb as DB
 
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-    state_name = sys.argv[4]
+    db_connect = DB.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
+    )
 
-    try:
-        conn = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=db_name)
-        cursor = conn.cursor()
-        cursor.execute("SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.state_id=states.id WHERE states.name=%s ORDER BY cities.id ASC", (state_name,))
-        rows = cursor.fetchall()
-        if not rows:
-            print("No cities found in state {}".format(state_name))
-        else:
-            for row in rows:
-                print(row)
-        cursor.close()
-        conn.close()
-    except MySQLdb.Error as e:
-        print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
-        sys.exit(1)
+    cmd = argv[4]
+
+    db_cursor = db_connect.cursor()
+
+    db_cursor.execute("SELECT cities.name FROM cities \
+                       INNER JOIN states ON states.id = cities.state_id \
+            WHERE states.name = %(name)s ORDER BY cities.id", {'name': cmd})
+    cities = db_cursor.fetchall()
+
+    first = 0
+    for city, in cities:
+        if first != 0:
+            print(", ", end="")
+        print(city, end="")
+        first += 1
+    print()
+    db_cursor.close()
+    db_connect.close()
