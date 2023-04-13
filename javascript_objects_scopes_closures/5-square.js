bcc0e60e3ce41ae52d53d1291ad648ddb518d86b
kiki3165 <kikikou82@gmail.com>
2023-04-13 16:45:22 +0000

    change code

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -1,34 +1,11 @@
 #!/usr/bin/node
 
-class Rectangle {
-  constructor (w, h) {
-    this.width = w;
-    this.height = h;
-  }
-  print () {
-    let row = '';
-    for (let i = 0; i < this.width; i++) {
-      row += 'X';
-    }
-    for (let j = 0; j < this.height; j++) {
-      console.log(row);
-    }
-  }
-  rotate () {
-    const temp = this.width;
-    this.width = this.height;
-    this.height = temp;
-  }
-  double() {
-    this.width *= 2;
-    this.height *= 2;
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
   }
 }
 
-  class Square extends Rectangle {
-    constructor(size) {
-      super(size, size);
-    }
-  }
-
-module.exports = Rectangle;
+module.exports = Square;
