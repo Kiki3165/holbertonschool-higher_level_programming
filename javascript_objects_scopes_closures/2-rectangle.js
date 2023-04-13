6b73a3f47e9966f9d32ff68f2ad9ad96b34efe24
kiki3165 <kikikou82@gmail.com>
2023-04-13 16:07:18 +0000

    change code

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -3,10 +3,9 @@
 class Rectangle {
   constructor (w, h) {
     if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
-      return Rectangle ;{};
-    }
     this.width = w;
     this.height = h;
+    }
     }
 }
 
