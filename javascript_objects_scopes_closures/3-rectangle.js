29c3bc4934e3668bf1066d4c67fd9621d16f575f
kiki3165 <kikikou82@gmail.com>
2023-04-13 16:21:51 +0000

    semistandard

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -1,21 +1,21 @@
 #!/usr/bin/node
 
 class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
       return {};
     }
     this.width = w;
     this.height = h;
-    }
-  
-  print() {
+  }
+
+  print () {
     let row = '';
     for (let i = 0; i < this.width; i++) {
       row += 'X';
     }
     for (let i = 0; i < this.height; i++) {
-    console.log(row);
+      console.log(row);
     }
   }
 }
