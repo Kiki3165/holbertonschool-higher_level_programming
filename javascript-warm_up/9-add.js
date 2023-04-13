f5a722e6f46d2c5f6b7c2c236a4b361042133ec3
kiki3165 <kikikou82@gmail.com>
2023-04-13 14:26:56 +0000

    semi

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -4,7 +4,7 @@ function add(a, b) {
   return a + b;
 }
 
-  const num1 = parseInt(process.argv[2]);
-  const num2 = parseInt(process.argv[3]);
+const num1 = parseInt(process.argv[2]);
+const num2 = parseInt(process.argv[3]);
 
-  console.log(add(num1, num2));
+console.log(add(num1, num2));
