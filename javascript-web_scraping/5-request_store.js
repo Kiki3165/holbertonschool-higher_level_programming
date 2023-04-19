556c4947d9772fe773cf3169352fb743309c6867
kiki3165 <kikikou82@gmail.com>
2023-04-19 20:18:14 +0000

    fix task5

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -13,8 +13,6 @@ request.get(url, (error, response, body) => {
     fs.writeFile(filePath, body, 'utf-8', (err) => {
       if (err) {
         console.error(err);
-      } else {
-        console.log(`File written to ${filePath}`);
       }
     });
   }
