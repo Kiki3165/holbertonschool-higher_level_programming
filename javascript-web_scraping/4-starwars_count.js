a97ae285f4f992690ac970e2eaa21235831a64fe
kiki3165 <kikikou82@gmail.com>
2023-04-19 20:06:49 +0000

    task4 change code

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,14 +1,23 @@
 #!/usr/bin/node
 
 const process = require('process');
-const id = process.argv[2];
+const url = process.argv[2];
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/' + id;
 
 request(url, (error, response, body) => {
   if (error) {
     console.log(error);
   } else {
-    console.log(JSON.parse(body).title);
+    const moviesList = JSON.parse(body).results;
+
+    let counter = 0;
+    for (const movies of moviesList) {
+      for (const character of movies.characters) {
+        if (character.includes('18')) {
+          counter++;
+        }
+      }
+    }
+    console.log(counter);
   }
 });
