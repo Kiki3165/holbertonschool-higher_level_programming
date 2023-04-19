3b53cf930fc849882f939d548ca74bffa2cdcd3b
kiki3165 <kikikou82@gmail.com>
2023-04-19 20:27:50 +0000

    task6

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const url = process.argv[2];
+
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    const todos = JSON.parse(body);
+    const completedTasks = {};
+
+    todos.forEach((todo) => {
+      if (todo.completed) {
+        if (completedTasks[todo.userId]) {
+          completedTasks[todo.userId]++;
+        } else {
+          completedTasks[todo.userId] = 1;
+        }
+      }
+    });
+
+    Object.keys(completedTasks).forEach((userId) => {
+      console.log(`User ${userId} completed ${completedTasks[userId]} tasks`);
+    });
+  }
+});
